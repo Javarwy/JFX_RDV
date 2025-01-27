fb5332fae2d38dad79ef29918ac66afc390cf951
Raphael Cazin <44144450+Javarwy@users.noreply.github.com>
2025-01-27 15:58:43 +0100

    [ADD} FournitureRepository + [UPT] stockView

---
+++ src/main/java/repository/FournitureRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import database.Database;
+import modeles.Fourniture;
+
+public class FournitureRepository {
+}

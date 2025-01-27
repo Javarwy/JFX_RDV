5a8ac121ddb5af968683a212b1aa2a36559804a7
Raphael Cazin <44144450+Javarwy@users.noreply.github.com>
2025-01-27 16:19:02 +0100

    [UPT] FournitureRepository

--- src/main/java/repository/FournitureRepository.java
+++ src/main/java/repository/FournitureRepository.java
@@ -9,4 +9,25 @@ import database.Database;
 import modeles.Fourniture;
 
 public class FournitureRepository {
-}
+    
+    public void getListFourniture(Fourniture fourniture) throws SQLException, IOException {
+        Database db = new Database();
+        Connection connection = db.getConnection();
+        String SQLReqFurn = "SELECT id_fourniture, libelle, description, prix, fournisseur FROM fourniture";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SQLReqFurn)) {
+            preparedStatement.setInt(1, fourniture.getId_fourniture());
+            preparedStatement.setString(2, fourniture.getLibelle());
+            preparedStatement.setString(3, fourniture.getDescription());
+            preparedStatement.setInt(4, fourniture.getPrix());
+            preparedStatement.setString(5, fourniture.getFournisseur());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Inscription Impossible", e);
+        } finally {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+
+
+}}

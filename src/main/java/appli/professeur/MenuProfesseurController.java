ad8280aee28e4830b9174ccd745c007a6e79b2ea
Yohan VIGNEZ <yohanvignez@gmail.com>
2025-01-28 14:05:29 +0100

    [FEAT] Ajout d'une page d'accueil pour Professeur

---
+++ src/main/java/appli/professeur/MenuProfesseurController.java
@@ -0,0 +1,43 @@
+package appli.professeur;
+
+import appli.StartApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import modeles.UtilisateurConnecte;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuProfesseurController implements Initializable {
+    @FXML
+    private Label welcomeText;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.welcomeText.setText("Bienvenue, "+UtilisateurConnecte.getInstance().getNom()+" "+UtilisateurConnecte.getInstance().getPrenom()+" !");
+    }
+    @FXML
+    protected void dossiers() throws IOException {
+
+    }
+    @FXML
+    protected void rendezVous() throws IOException {
+
+    }
+    @FXML
+    protected void demandeFourniture() throws IOException {
+
+    }
+    @FXML
+    protected void modifierInfos() throws IOException {
+        StartApplication.changeScene("accueil/modifierInfosView.fxml");
+    }
+
+    @FXML
+    protected void deconnexion() throws IOException {
+        UtilisateurConnecte.clearInstance();
+        StartApplication.changeScene("accueil/loginView.fxml");
+    }
+}

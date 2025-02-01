27fbab6b6bb4af93cc43e13cf4b56d1c8fc5c4fd
Yohan VIGNEZ <yohanvignez@gmail.com>
2025-02-01 22:31:40 +0100

    [FEAT] Création du repository pour le model Dossier

---
+++ src/main/java/repository/DossierRepository.java
@@ -0,0 +1,22 @@
+package repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import database.Database;
+import modeles.Dossier;
+import modeles.Etudiant;
+
+public class DossierRepository {
+    public ArrayList<Dossier> getDossiers() throws SQLException {
+        Database db = new Database();
+        PreparedStatement ps = db.getConnection().prepareStatement("SELECT d.id_dossier, d.date, d.heure, d.filliere, d.motivation, e.id_etudiant, e.prenomEtudiant, e.nomEtudiant, e.diplome, e.emailEtudiant, e.telephone FROM dossier as d INNER JOIN etudiant as e ON d.ref_etudiant = e.id_etudiant;");
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Dossier> dossiers = new ArrayList<>();
+        while(rs.next()) {
+            Etudiant etudiant = new Etudiant(rs.getInt(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11));
+            dossiers.add(new Dossier(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), etudiant));
+        }
+        return dossiers;
+    }
+}

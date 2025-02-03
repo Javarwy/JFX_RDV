2b0529ec16e7f847afcd66dc615f270860892d98
aurelien30 <aurassil3003@gmail.com>
2025-02-03 14:10:15 +0100

    [FIX] changement de la  bdd

--- jfx.sql
+++ jfx.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 20 jan. 2025 à 13:09
--- Version du serveur : 8.0.31
--- Version de PHP : 8.2.0
+-- Généré le : sam. 01 fév. 2025 à 21:35
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,15 +29,22 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `dossier`;
 CREATE TABLE IF NOT EXISTS `dossier` (
-  `id_dossier` int NOT NULL,
+  `id_dossier` int NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `heure` time NOT NULL,
   `filliere` varchar(25) NOT NULL,
   `motivation` text NOT NULL,
-  `ref_rdv` int NOT NULL,
+  `ref_etudiant` int NOT NULL,
   PRIMARY KEY (`id_dossier`),
-  KEY `ref_rdv` (`ref_rdv`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table sur les dossiers';
+  KEY `ref_etudiant` (`ref_etudiant`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table sur les dossiers';
+
+--
+-- Déchargement des données de la table `dossier`
+--
+
+INSERT INTO `dossier` (`id_dossier`, `date`, `heure`, `filliere`, `motivation`, `ref_etudiant`) VALUES
+(1, '2025-02-01', '21:41:44', 'BTS SIO option SLAM', 'Je suis motivé.', 1);
 
 -- --------------------------------------------------------
 
@@ -47,13 +54,21 @@ CREATE TABLE IF NOT EXISTS `dossier` (
 
 DROP TABLE IF EXISTS `etudiant`;
 CREATE TABLE IF NOT EXISTS `etudiant` (
-  `id_etudiant` int NOT NULL,
+  `id_etudiant` int NOT NULL AUTO_INCREMENT,
+  `nomEtudiant` varchar(50) NOT NULL,
   `prenomEtudiant` varchar(50) NOT NULL,
   `diplome` varchar(25) NOT NULL,
   `emailEtudiant` varchar(50) NOT NULL,
-  `telephone` int NOT NULL,
+  `telephone` varchar(10) NOT NULL,
   PRIMARY KEY (`id_etudiant`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table des étudiants';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table des étudiants';
+
+--
+-- Déchargement des données de la table `etudiant`
+--
+
+INSERT INTO `etudiant` (`id_etudiant`, `nomEtudiant`, `prenomEtudiant`, `diplome`, `emailEtudiant`, `telephone`) VALUES
+(1, 'Etudiant', 'Etudiant', 'STI2D', 'etudiant@etudiant.fr', '0600000000');
 
 -- --------------------------------------------------------
 
@@ -63,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `etudiant` (
 
 DROP TABLE IF EXISTS `fourniture`;
 CREATE TABLE IF NOT EXISTS `fourniture` (
-  `id_fourniture` int NOT NULL,
+  `id_fourniture` int NOT NULL AUTO_INCREMENT,
   `libelle` varchar(20) NOT NULL,
   `description` varchar(100) NOT NULL,
   `prix` int NOT NULL,
@@ -79,10 +94,12 @@ CREATE TABLE IF NOT EXISTS `fourniture` (
 
 DROP TABLE IF EXISTS `rendezvous`;
 CREATE TABLE IF NOT EXISTS `rendezvous` (
-  `id_rendezvous` int NOT NULL,
+  `id_rendezvous` int NOT NULL AUTO_INCREMENT,
   `date_rendezvous` date NOT NULL,
   `heure_rendezvous` time NOT NULL,
-  PRIMARY KEY (`id_rendezvous`)
+  `ref_dossier` int NOT NULL,
+  PRIMARY KEY (`id_rendezvous`),
+  KEY `ref_dossier` (`ref_dossier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table Rendez Vous';
 
 -- --------------------------------------------------------
@@ -93,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `rendezvous` (
 
 DROP TABLE IF EXISTS `salle`;
 CREATE TABLE IF NOT EXISTS `salle` (
-  `id_salle` int NOT NULL,
+  `id_salle` int NOT NULL AUTO_INCREMENT,
   `nom_salle` varchar(20) NOT NULL,
   `occupe` tinyint(1) NOT NULL DEFAULT '0',
   `professeur_present` int NOT NULL,
@@ -111,15 +128,23 @@ CREATE TABLE IF NOT EXISTS `salle` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id_utilisateur` int NOT NULL,
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) NOT NULL,
   `prenom` varchar(30) NOT NULL,
   `email` varchar(40) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
   `role` varchar(20) NOT NULL,
-  `fourni_com` int NOT NULL,
+  `fourni_com` int DEFAULT NULL,
   PRIMARY KEY (`id_utilisateur`),
   KEY `fourni_com` (`fourni_com`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table de l''utilisateur';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table de l''utilisateur';
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_utilisateur`, `nom`, `prenom`, `email`, `mdp`, `role`, `fourni_com`) VALUES
+(1, 'a', 'a', 'a', '$2a$10$1ScMjQ21fUgXibYYIdMPf.zBdRIrGYkYVj5j..HpSKvZcUq1MWcbS', 'Professeur', NULL);
 
 --
 -- Contraintes pour les tables déchargées
@@ -129,20 +154,26 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 -- Contraintes pour la table `dossier`
 --
 ALTER TABLE `dossier`
-  ADD CONSTRAINT `dossier_on_rdv` FOREIGN KEY (`ref_rdv`) REFERENCES `dossier` (`id_dossier`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `etudiant_on_dossier` FOREIGN KEY (`ref_etudiant`) REFERENCES `etudiant` (`id_etudiant`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
+  ADD CONSTRAINT `dossier_on_rdv` FOREIGN KEY (`ref_dossier`) REFERENCES `dossier` (`id_dossier`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Contraintes pour la table `salle`
 --
 ALTER TABLE `salle`
-  ADD CONSTRAINT `salle_on_prof` FOREIGN KEY (`professeur_present`) REFERENCES `utilisateur` (`id_utilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `salle_on_rdv` FOREIGN KEY (`rdv_ref`) REFERENCES `salle` (`id_salle`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `salle_on_prof` FOREIGN KEY (`professeur_present`) REFERENCES `utilisateur` (`id_utilisateur`),
+  ADD CONSTRAINT `salle_on_rdv` FOREIGN KEY (`rdv_ref`) REFERENCES `rendezvous` (`id_rendezvous`);
 
 --
 -- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `util_on_fourn` FOREIGN KEY (`fourni_com`) REFERENCES `fourniture` (`id_fourniture`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `util_on_fourn` FOREIGN KEY (`fourni_com`) REFERENCES `fourniture` (`id_fourniture`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

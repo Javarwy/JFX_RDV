950f3714ba084f766a6df7544530d7b498cf7f88
Yohan VIGNEZ <yohanvignez@gmail.com>
2025-02-27 19:34:44 +0100

    [FEAT] Ajout d'un outil de recherche par filtre sur la page Demande de fournitures des professeurs

--- src/main/java/appli/professeur/DemandesProfesseurController.java
+++ src/main/java/appli/professeur/DemandesProfesseurController.java
@@ -1,6 +1,7 @@
 package appli.professeur;
 
 import appli.StartApplication;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -8,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import modeles.DemandeFourniture;
 import modeles.UtilisateurConnecte;
 import repository.DemandeFournitureRepository;
@@ -26,9 +28,32 @@ public class DemandesProfesseurController implements Initializable {
     private Label erreur;
     @FXML
     private Button nouvelleDemande;
+    @FXML
+    private VBox rechercheAvancee;
+    @FXML
+    private TextField libelleRecherche;
+    @FXML
+    private TextField descriptionRecherche;
+    @FXML
+    private Spinner<Double> prixDebutRecherche;
+    @FXML
+    private Spinner<Double> prixFinRecherche;
+    @FXML
+    private TextField fournisseurRecherche;
+    @FXML
+    private Spinner<Integer> quantiteDebutRecherche;
+    @FXML
+    private Spinner<Integer> quantiteFinRecherche;
+    @FXML
+    private TextField raisonRecherche;
+    @FXML
+    private ComboBox<String> statutRecherche;
 
     // Données de la colonne sélectionnée depuis le TableView
     private DemandeFourniture demandeSel;
+
+    // Liste des demandes de fournitures
+    private ArrayList<DemandeFourniture> demandes;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -38,23 +63,56 @@ public class DemandesProfesseurController implements Initializable {
                 {"Quantité", "quantite"},
                 {"Raison", "raison"},
                 {"Statut", "statut"},
-                {"Fourniture", "refFourniture"}
+                {"Id. fourniture", "id_fourniture"},
+                {"Libellé", "libelle"},
+                {"Description", "description"},
+                {"Prix (€)", "prix"},
+                {"Fournisseur", "fournisseur"}
         };
         // Parcours de l'ensemble des colonnes
         for (int i = 0; i < colonnes.length; i++) {
             TableColumn<DemandeFourniture,String> maColonne = new TableColumn<>(colonnes[i][0]);
             maColonne.setCellValueFactory(new PropertyValueFactory<DemandeFourniture,String>(colonnes[i][1]));
+            switch(colonnes[i][1]){
+                case "id_fourniture":
+                    maColonne.setVisible(false);
+                    maColonne.setCellValueFactory(cellData ->
+                            new SimpleStringProperty(String.valueOf(cellData.getValue().getRefFourniture().getId_fourniture()))
+                    );
+                    break;
+                case "libelle":
+                    maColonne.setCellValueFactory(cellData ->
+                            new SimpleStringProperty(cellData.getValue().getRefFourniture().getLibelle())
+                    );
+                    break;
+                case "description":
+                    maColonne.setCellValueFactory(cellData ->
+                            new SimpleStringProperty(cellData.getValue().getRefFourniture().getDescription())
+                    );
+                    break;
+                case "prix":
+                    maColonne.setCellValueFactory(cellData ->
+                            new SimpleStringProperty(String.valueOf(cellData.getValue().getRefFourniture().getPrix()))
+                    );
+                    break;
+                case "fournisseur":
+                    maColonne.setCellValueFactory(cellData ->
+                            new SimpleStringProperty(cellData.getValue().getRefFourniture().getFournisseur())
+                    );
+                    break;
+                default:
+                    break;
+            }
             tableauDemandes.getColumns().add(maColonne);
         }
         DemandeFournitureRepository demandeFournitureRepository = new DemandeFournitureRepository();
-        ArrayList<DemandeFourniture> demandes;
         try {
-            demandes = demandeFournitureRepository.getDemandeFournituresByUtilisateur(UtilisateurConnecte.getInstance());
+            this.demandes = demandeFournitureRepository.getDemandeFournituresByUtilisateur(UtilisateurConnecte.getInstance());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         // Tri de la liste pour mettre les demandes au statut "En cours de validation" en premier dans la liste
-        demandes.sort((d1, d2) -> {
+        this.demandes.sort((d1, d2) -> {
             if (d1.getStatut().equals("En cours de validation") && !d2.getStatut().equals("En cours de validation")) {
                 return -1;
             } else if (!d1.getStatut().equals("En cours de validation") && d2.getStatut().equals("En cours de validation")) {
@@ -65,7 +123,10 @@ public class DemandesProfesseurController implements Initializable {
         });
         // Ajoute les données de la liste dans les colonnes du TableView
         ObservableList<DemandeFourniture> observableList = tableauDemandes.getItems();
-        observableList.setAll(demandes);
+        observableList.setAll(this.demandes);
+        this.prixDebutRecherche.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 99999.99, 0.0, 0.01));
+        this.prixFinRecherche.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 99999.99, 0.0, 0.01));
+        this.statutRecherche.getItems().addAll(null, "En cours de validation", "Refusée", "Acceptée");
     }
     @FXML
     protected void onDemandeSelection(MouseEvent event) {
@@ -84,6 +145,54 @@ public class DemandesProfesseurController implements Initializable {
     @FXML
     protected void nouvelleDemande() throws IOException {
         StartApplication.changeScene("professeur/nouvelleDemandeProfesseurView.fxml");
+    }
+
+    @FXML
+    protected void activerRecherche() {
+        if (this.rechercheAvancee.isVisible()) {
+            this.rechercheAvancee.setVisible(false);
+            this.rechercheAvancee.setManaged(false);
+        } else {
+            this.rechercheAvancee.setVisible(true);
+            this.rechercheAvancee.setManaged(true);
+        }
+    }
+
+    @FXML
+    protected void filtrage() throws SQLException {
+        if (this.libelleRecherche == null && this.descriptionRecherche == null && this.prixDebutRecherche == null && this.prixFinRecherche == null && this.fournisseurRecherche == null && this.quantiteDebutRecherche == null && this.quantiteFinRecherche == null && this.raisonRecherche == null && this.statutRecherche == null) {
+            System.out.println("Aucun filtre.");
+        } else {
+            String libelle = this.libelleRecherche.getText();
+            String description = this.descriptionRecherche.getText();
+            double prixDebut = this.prixDebutRecherche.getValue();
+            double prixFin = this.prixFinRecherche.getValue();
+            int quantiteDebut = this.quantiteDebutRecherche.getValue();
+            int quantiteFin = this.quantiteFinRecherche.getValue();
+            String fournisseur = this.fournisseurRecherche.getText();
+            String raison = this.raisonRecherche.getText();
+            String statut = this.statutRecherche.getValue() != null ? this.statutRecherche.getValue() : "";
+            ArrayList<DemandeFourniture> demandeFournitures;
+            demandeFournitures = new DemandeFournitureRepository().getMesDemandeFournituresFiltres(libelle, description, prixDebut, prixFin, quantiteDebut, quantiteFin, fournisseur, raison, statut, UtilisateurConnecte.getInstance());
+            ObservableList<DemandeFourniture> observableList = tableauDemandes.getItems();
+            // Appliquer sur le tableau les résultats filtrés par la requête
+            observableList.setAll(demandeFournitures);
+        }
+    }
+
+    @FXML
+    protected void resetFiltres() {
+        this.libelleRecherche.clear();
+        this.descriptionRecherche.clear();
+        this.prixDebutRecherche.getValueFactory().setValue(0.00);
+        this.prixFinRecherche.getValueFactory().setValue(0.00);
+        this.quantiteDebutRecherche.getValueFactory().setValue(0);
+        this.quantiteFinRecherche.getValueFactory().setValue(0);
+        this.fournisseurRecherche.clear();
+        this.raisonRecherche.clear();
+        this.statutRecherche.getSelectionModel().clearSelection();
+        ObservableList<DemandeFourniture> observableList = tableauDemandes.getItems();
+        observableList.setAll(this.demandes);
     }
 
     @FXML
--- src/main/java/repository/DemandeFournitureRepository.java
+++ src/main/java/repository/DemandeFournitureRepository.java
@@ -55,4 +55,48 @@ public class DemandeFournitureRepository {
         }
         return demandes;
     }
+
+    public ArrayList<DemandeFourniture> getMesDemandeFournituresFiltres(String libelle, String description, double prixDebut, double prixFin, int quantiteDebut, int quantiteFin, String fournisseur, String raison, String statut, Utilisateur utilisateur) throws SQLException {
+        Database db = new Database();
+        PreparedStatement ps = db.getConnection().prepareStatement(
+                "SELECT df.id_demandefourniture, df.quantite, df.raison, df.statut, df.ref_fourniture, f.libelle, f.description, f.prix, f.fournisseur FROM demandefourniture as df INNER JOIN fourniture as f ON f.id_fourniture = df.ref_fourniture WHERE df.ref_utilisateur = ? AND f.libelle LIKE ? AND f.description LIKE ? AND f.prix BETWEEN ? AND ? AND df.quantite BETWEEN ? AND ? AND f.fournisseur LIKE ? AND df.raison LIKE ? AND df.statut LIKE ?"
+        );
+        ps.setInt(1, utilisateur.getId_utilisateur());
+        if (!libelle.isEmpty()) {
+            ps.setString(2, "%"+libelle+"%");
+        } else {
+            ps.setString(2, "%");
+        }
+        if (!description.isEmpty()) {
+            ps.setString(3, "%"+description+"%");
+        } else {
+            ps.setString(3, "%");
+        }
+        ps.setDouble(4, prixDebut);
+        ps.setDouble(5, prixFin);
+        ps.setInt(6, quantiteDebut);
+        ps.setInt(7, quantiteFin);
+        if (!fournisseur.isEmpty()) {
+            ps.setString(8, "%"+fournisseur+"%");
+        } else {
+            ps.setString(8, "%");
+        }
+        if (!statut.isEmpty()) {
+            ps.setString(9, "%"+statut+"%");
+        } else {
+            ps.setString(9, "%");
+        }
+        if (!statut.isEmpty()) {
+            ps.setString(10, "%"+statut+"%");
+        } else {
+            ps.setString(10, "%");
+        }
+        ResultSet rs = ps.executeQuery();
+        ArrayList<DemandeFourniture> demandes = new ArrayList<>();
+        while (rs.next()) {
+            Fourniture fourniture = new Fourniture(rs.getInt("ref_fourniture"), rs.getString("libelle"), rs.getString("description"), rs.getDouble("prix"), rs.getString("fournisseur"));
+            demandes.add(new DemandeFourniture(rs.getInt("id_demandefourniture"), rs.getInt("quantite"), rs.getString("raison"), rs.getString("statut"), fourniture, utilisateur));
+        }
+        return demandes;
+    }
 }

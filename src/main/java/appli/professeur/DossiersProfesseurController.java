81cbeac66e0171dddccffdb932783eff84ba00db
Yohan VIGNEZ <yohanvignez@gmail.com>
2025-02-27 17:36:33 +0100

    [FEAT] Ajout d'un outil de recherche par filtre sur la page Dossiers d'inscription des professeurs

--- src/main/java/appli/professeur/DossiersProfesseurController.java
+++ src/main/java/appli/professeur/DossiersProfesseurController.java
@@ -1,23 +1,23 @@
 package appli.professeur;
 
 import appli.StartApplication;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import modeles.Dossier;
 import repository.DossierRepository;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -29,6 +29,38 @@ public class DossiersProfesseurController implements Initializable {
     private Label erreur;
     @FXML
     private Button rdv;
+    @FXML
+    private VBox rechercheAvancee;
+    @FXML
+    private DatePicker dateDebut;
+    @FXML
+    private DatePicker dateFin;
+    @FXML
+    private Spinner<Integer> heureDebut;
+    @FXML
+    private Spinner<Integer> minuteDebut;
+    @FXML
+    private Spinner<Integer> heureFin;
+    @FXML
+    private Spinner<Integer> minuteFin;
+    @FXML
+    private ComboBox<String> filiereRecherche;
+    @FXML
+    private TextField motivationRecherche;
+    @FXML
+    private TextField nomRecherche;
+    @FXML
+    private TextField prenomRecherche;
+    @FXML
+    private ComboBox<String> diplomeRecherche;
+    @FXML
+    private TextField emailRecherche;
+    @FXML
+    private TextField telRecherche;
+    @FXML
+    private Button filtrer;
+    @FXML
+    private Button reset;
 
     // Données de la colonne sélectionnée depuis le TableView
     private Dossier dossierSel;
@@ -58,37 +90,37 @@ public class DossiersProfesseurController implements Initializable {
                 case "id_etudiant":
                     maColonne.setVisible(false);
                     maColonne.setCellValueFactory(cellData ->
-                            new javafx.beans.property.SimpleStringProperty(String.valueOf(cellData.getValue().getRefEtudiant().getId_etudiant()))
+                            new SimpleStringProperty(String.valueOf(cellData.getValue().getRefEtudiant().getId_etudiant()))
                     );
                     break;
                 // Pour la colonne "nomEtudiant", insère le nom de l'étudiant
                 case "nomEtudiant":
                     maColonne.setCellValueFactory(cellData ->
-                            new javafx.beans.property.SimpleStringProperty(cellData.getValue().getRefEtudiant().getNomEtudiant())
+                            new SimpleStringProperty(cellData.getValue().getRefEtudiant().getNomEtudiant())
                     );
                     break;
                 // Pour la colonne "prenomEtudiant", insère le prénom de l'étudiant
                 case "prenomEtudiant":
                     maColonne.setCellValueFactory(cellData ->
-                            new javafx.beans.property.SimpleStringProperty(cellData.getValue().getRefEtudiant().getPrenomEtudiant())
+                            new SimpleStringProperty(cellData.getValue().getRefEtudiant().getPrenomEtudiant())
                     );
                     break;
                 // Pour la colonne "diplome", insère le diplôme de l'étudiant
                 case "diplome":
                     maColonne.setCellValueFactory(cellData ->
-                            new javafx.beans.property.SimpleStringProperty(cellData.getValue().getRefEtudiant().getDiplome())
+                            new SimpleStringProperty(cellData.getValue().getRefEtudiant().getDiplome())
                     );
                     break;
                 // Pour la colonne "emailEtudiant", insère l'email de l'étudiant
                 case "emailEtudiant":
                     maColonne.setCellValueFactory(cellData ->
-                            new javafx.beans.property.SimpleStringProperty(cellData.getValue().getRefEtudiant().getEmailEtudiant())
+                            new SimpleStringProperty(cellData.getValue().getRefEtudiant().getEmailEtudiant())
                     );
                     break;
                 // Pour la colonne "telephone", insère le téléphone de l'étudiant
                 case "telephone":
                     maColonne.setCellValueFactory(cellData ->
-                            new javafx.beans.property.SimpleStringProperty(cellData.getValue().getRefEtudiant().getTelephone())
+                            new SimpleStringProperty(cellData.getValue().getRefEtudiant().getTelephone())
                     );
                     break;
                 default:
@@ -98,14 +130,25 @@ public class DossiersProfesseurController implements Initializable {
         }
         DossierRepository dossierRepository = new DossierRepository();
         ArrayList<Dossier> dossiers;
+        ArrayList<String> filieres;
         try {
             dossiers = dossierRepository.getDossiers();
+            filieres = dossierRepository.getFilieres();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         // Ajoute les données de la liste dans les colonnes du TableView
         ObservableList<Dossier> observableList = tableauDossier.getItems();
         observableList.setAll(dossiers);
+        this.filiereRecherche.getItems().add(null);
+        this.diplomeRecherche.getItems().add(null);
+        for (String s : filieres){
+            this.filiereRecherche.getItems().add(s);
+        }
+        this.dateDebut.setValue(LocalDate.now());
+        this.dateFin.setValue(LocalDate.now());
+        this.heureFin.getValueFactory().setValue(23);
+        this.minuteFin.getValueFactory().setValue(59);
     }
     @FXML
     protected void onDossierSelection(MouseEvent event) throws IOException, SQLException {
@@ -145,6 +188,61 @@ public class DossiersProfesseurController implements Initializable {
     @FXML
     protected void prendreRdv() throws IOException {
         StartApplication.changeScene("professeur/prendreRdvProfesseurView.fxml", new PrendreRdvProfesseurController(this.dossierSel));
+    }
+
+    @FXML
+    protected void activerRecherche() {
+        if (this.rechercheAvancee.isVisible()) {
+            this.rechercheAvancee.setVisible(false);
+            this.rechercheAvancee.setManaged(false);
+        } else {
+            this.rechercheAvancee.setVisible(true);
+            this.rechercheAvancee.setManaged(true);
+        }
+    }
+
+    @FXML
+    protected void filtrage() throws SQLException {
+        if (this.dateDebut == null && this.dateFin == null && this.heureDebut == null && this.heureFin == null && this.filiereRecherche == null && this.motivationRecherche == null && this.nomRecherche == null && this.prenomRecherche == null && this.diplomeRecherche == null && this.emailRecherche == null && this.telRecherche == null) {
+            System.out.println("Aucun filtre.");
+        } else {
+            String dateDebut = this.dateDebut.getValue() != null ? this.dateDebut.getValue().toString() : "";
+            String dateFin = this.dateFin.getValue() != null ? this.dateFin.getValue().toString() : "";
+            String heureDebut = this.heureDebut.getValue() != null ? this.heureDebut.getValue().toString() : "";
+            String minuteDebut = this.minuteDebut.getValue() != null ? this.minuteDebut.getValue().toString() : "";
+            LocalTime timeDebut = LocalTime.of(Integer.parseInt(heureDebut), Integer.parseInt(minuteDebut), 0);
+            String heureFin = this.heureFin.getValue() != null ? this.heureFin.getValue().toString() : "";
+            String minuteFin = this.minuteFin.getValue() != null ? this.minuteFin.getValue().toString() : "";
+            LocalTime timeFin = LocalTime.of(Integer.parseInt(heureFin), Integer.parseInt(minuteFin), 0);
+            String filiere = this.filiereRecherche.getValue() != null ? this.filiereRecherche.getValue() : "";
+            String motivation = this.motivationRecherche.getText();
+            String nom = this.nomRecherche.getText();
+            String prenom = this.prenomRecherche.getText();
+            String diplome = this.diplomeRecherche.getValue() != null ? this.diplomeRecherche.getValue() : "";
+            String email = this.emailRecherche.getText();
+            String tel = this.telRecherche.getText();
+            ArrayList<Dossier> dossiers;
+            dossiers = new DossierRepository().getDossiersFiltres(dateDebut, dateFin, timeDebut.toString(), timeFin.toString(), filiere, motivation, nom, prenom, diplome, email, tel);
+            ObservableList<Dossier> observableList = tableauDossier.getItems();
+            // Appliquer sur le tableau les résultats filtrés par la requête
+            observableList.setAll(dossiers);
+        }
+    }
+
+    @FXML
+    protected void resetFiltres() {
+        this.dateDebut.setValue(LocalDate.now());
+        this.dateFin.setValue(LocalDate.now());
+        this.heureDebut.getValueFactory().setValue(0);
+        this.minuteDebut.getValueFactory().setValue(0);
+        this.heureFin.getValueFactory().setValue(23);
+        this.minuteFin.getValueFactory().setValue(59);
+        this.filiereRecherche.getSelectionModel().clearSelection();
+        this.nomRecherche.clear();
+        this.prenomRecherche.clear();
+        this.diplomeRecherche.getSelectionModel().clearSelection();
+        this.emailRecherche.clear();
+        this.telRecherche.clear();
     }
 
     @FXML
--- src/main/java/repository/DossierRepository.java
+++ src/main/java/repository/DossierRepository.java
@@ -21,6 +21,90 @@ public class DossierRepository {
         return dossiers;
     }
 
+    // Récupère une liste des dossiers correspondants aux filtres
+    public ArrayList<Dossier> getDossiersFiltres(String dateDebut, String dateFin, String heureDebut, String heureFin, String filiere, String motivation, String nom, String prenom, String diplome, String email, String telephone) throws SQLException {
+        Database db = new Database();
+        PreparedStatement ps = db.getConnection().prepareStatement(
+          "SELECT d.id_dossier, d.date, d.heure, d.filliere, d.motivation, e.id_etudiant, e.prenomEtudiant, e.nomEtudiant, e.diplome, e.emailEtudiant, e.telephone FROM dossier as d INNER JOIN etudiant as e ON d.ref_etudiant = e.id_etudiant WHERE d.date BETWEEN ? AND ? AND d.heure BETWEEN ? AND ? AND d.filliere LIKE ? AND d.motivation LIKE ? AND e.nomEtudiant LIKE ? AND e.prenomEtudiant LIKE ? AND e.diplome LIKE ? AND e.emailEtudiant LIKE ? AND e.telephone LIKE ?"
+        );
+        if (!dateDebut.isEmpty()){
+            ps.setString(1, dateDebut);
+        } else {
+            ps.setString(1, "1900-01-01");
+        }
+        if (!dateFin.isEmpty()){
+            ps.setString(2, dateFin);
+        } else {
+            ps.setString(2, "2100-01-01");
+        }
+        if (!heureDebut.isEmpty()){
+            ps.setString(3, heureDebut+":00");
+        } else {
+            ps.setString(3, "08:00:00");
+        }
+        if (!heureFin.isEmpty()){
+            ps.setString(4, heureFin+":00");
+        } else {
+            ps.setString(4, "18:00:00");
+        }
+        if (!filiere.isEmpty()){
+            ps.setString(5, "%"+filiere+"%");
+        } else {
+            ps.setString(5, "%");
+        }
+        if (!motivation.isEmpty()){
+            ps.setString(6, "%"+motivation+"%");
+        } else {
+            ps.setString(6, "%");
+        }
+        if (!nom.isEmpty()){
+            ps.setString(7, "%"+nom+"%");
+        } else {
+            ps.setString(7, "%");
+        }
+        if (!prenom.isEmpty()){
+            ps.setString(8, "%"+prenom+"%");
+        } else {
+            ps.setString(8, "%");
+        }
+        if (!diplome.isEmpty()){
+            ps.setString(9, "%"+diplome+"%");
+        } else {
+            ps.setString(9, "%");
+        }
+        if (!email.isEmpty()){
+            ps.setString(10, "%"+email+"%");
+        } else {
+            ps.setString(10, "%");
+        }
+        if (!telephone.isEmpty()){
+            ps.setString(11, "%"+telephone+"%");
+        } else {
+            ps.setString(11, "%");
+        }
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Dossier> dossiers = new ArrayList<>();
+        while(rs.next()) {
+            Etudiant etudiant = new Etudiant(rs.getInt(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11));
+            dossiers.add(new Dossier(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), etudiant));
+        }
+        return dossiers;
+    }
+
+    // Récupère la liste de toutes les filières demandées dans les dossiers
+    public ArrayList<String> getFilieres() throws SQLException {
+        Database db = new Database();
+        PreparedStatement ps = db.getConnection().prepareStatement(
+          "SELECT DISTINCT filliere FROM dossier ORDER BY filliere ASC;"
+        );
+        ResultSet rs = ps.executeQuery();
+        ArrayList<String> filieres = new ArrayList<>();
+        while(rs.next()){
+            filieres.add(rs.getString(1));
+        }
+        return filieres;
+    }
+
     // Vérifie si un dosiser est déjà pris en charge (rendez-vous déjà créé) ou non
     public boolean estPrisEnCharge(int refDossier) throws SQLException {
         Database db = new Database();

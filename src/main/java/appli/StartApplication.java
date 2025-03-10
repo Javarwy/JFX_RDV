0c5e6ae766202da073ab7a6c34b02386fa525529
Yohan VIGNEZ <yohanvignez@gmail.com>
2025-03-10 15:36:33 +0100

    [FEAT] Agrandissement automatique de la fenêtre d'application

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -15,31 +15,34 @@ public class StartApplication extends Application {
     public void start(Stage stage) throws IOException {
         mainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/loginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(StartApplication.class.getResource("styles/style.css").toExternalForm());
         mainStage.getIcons().add(new Image(StartApplication.class.getResourceAsStream("images/logo-schuman-short.png")));
         mainStage.setTitle("Connexion");
         mainStage.setScene(scene);
         mainStage.show();
+        mainStage.setMaximized(true);
     }
 
     public static void changeScene(String nomDuFichierFxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        Scene scene = new Scene(fxmlLoader.load(), mainStage.getScene().getWidth(), mainStage.getScene().getHeight());
         scene.getStylesheets().add(StartApplication.class.getResource("styles/style.css").toExternalForm());
         mainStage.setScene(scene);
+        mainStage.show();
+        mainStage.setMaximized(true);
     }
 
     public static void changeScene(String nomDuFichierFxml, Object controller){
-        mainStage.close();
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml));
             fxmlLoader.setController(controller);
             Parent root = fxmlLoader.load();
-            Scene scene = new Scene(root, 1600, 900);
+            Scene scene = new Scene(root, mainStage.getScene().getWidth(), mainStage.getScene().getHeight());
             scene.getStylesheets().add(StartApplication.class.getResource("styles/style.css").toExternalForm());
             mainStage.setScene(scene);
             mainStage.show();
+            mainStage.setMaximized(true);
         } catch (IOException e) {
             System.err.println(String.format("Error: %s", e.getMessage()));
         }
